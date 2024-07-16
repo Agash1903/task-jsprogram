a2375ada026c5535a6b30bd5b7c7e3d3f8f52b8e
Agash. A <141730950+Agash1903@users.noreply.github.com>
2024-07-16 16:51:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// Array creation and Intialiation
+let array = [1,2,3,4,5];
+console.log("Array Creation and Intialization");
+console.log(array)
+
+// Array Searching
+var arr= [ 2,3,7,2,5];
+console.log("Array Searhing");
+console.log(arr.indexOf(5));
+
+// max array
+let arrMax= [4,3,7,9,2]
+console.log("Max Array");
+console.log(Math.max(...arrMax));
+
+// min array
+
+let arrMin= [4,3,7,9,2]
+console.log("Min Array");
+console.log(Math.min(...arrMin));
+
+
+// Array Checking
+
+var arr= [ 2,3,7,2,5];
+console.log("Array Chicking");
+console.log(arr.includes(9));
+
+// Array push
+let array1= [2,4,5,6,9,2];
+console.log("Array push");
+console.log(array1.push(10));
+
+// Array pop
+
+let array2= [2,4,5,6,9,2];
+console.log("Array pop");
+console.log(array2.pop());
+
+// Array shift
+
+let array3= [2,4,5,6,9,2];
+console.log("Array Shift number");
+console.log(array3.shift(3));
+console.log(array3);
+
+// Array Reverse
+
+let array4= [1,2,3,4,5,6,7,8,9];
+console.log("Array Reverse");
+console.log(array4.reverse());
+
+// Array Concat
+
+let array5= [1,2,3,4,5,6];
+let array6= [7,8,9];
+console.log("Array Concat");
+console.log(array5.concat(array6));
+
+// Array split
+
+let array7= "Hello, World!";
+console.log("Array Split");
+console.log(array7.split(", "));
+
+
